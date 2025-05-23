4596cf5e4bb4792f236526c6b99ea515e3c7c907
Radjznairene Jallorina <rjallorina@myseneca.ca>
2025-05-23 12:57:25 -0400

    lab2f

---
+++ lab2f.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+# Author: Radjznairene Jallorina
+# Author ID: 144342235
+# Date Created: 2025/05/23
+
+import sys
+
+timer = int(sys.argv[1])
+
+while timer > 0:
+    print(timer)
+    timer -= 1
+
+print("blast off!")
+
